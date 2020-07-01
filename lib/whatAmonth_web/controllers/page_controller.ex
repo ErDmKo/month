8d7c13575e9c9f00a4d647721202b9d85de57dda
erdmko <erdmko@gmail.com>
2020-07-01 13:05:32 +0300

    Google lighthouse review

--- lib/whatAmonth_web/controllers/page_controller.ex
+++ lib/whatAmonth_web/controllers/page_controller.ex
@@ -6,8 +6,13 @@ defmodule WhatAmonthWeb.PageController do
     no = Integer.parse(id)
     case no do
       {monthNo, ""} when monthNo < 13 and monthNo >=1 ->
+        nowDate = Timex.now()
+        year = if nowDate.month > monthNo, do: nowDate.year + 1, else: nowDate.year
+        date = Timex.DateTime.Helpers.construct({year, monthNo, 1}, nowDate.time_zone)
         render(conn, "index.html", %{
-          month: monthNo
+          month: monthNo,
+          year: year,
+          days: Timex.days_in_month(date)
         })
       _ -> conn
         |> put_status(:not_found)
@@ -17,9 +22,12 @@ defmodule WhatAmonthWeb.PageController do
   end
 
   def index(conn, _param) do
+    date = Timex.today()
     render(conn, "index.html", %{
       isIndex: true,
-      month: Timex.today().month
+      month: date.month,
+      year: date.year,
+      days: Timex.days_in_month(date)
     })
   end
 end
--- lib/whatAmonth_web/views/layout_view.ex
+++ lib/whatAmonth_web/views/layout_view.ex
@@ -2,6 +2,18 @@ defmodule WhatAmonthWeb.LayoutView do
   use WhatAmonthWeb, :view
   require Logger
 
+  def description(%{:isIndex => true}) do
+    ~s/Номера месяцев в году/
+  end
+
+  def description(%{:month => month}) do
+    ~s/Номер месяца #{getTitle(month)} в году/
+  end
+
+  def description(_a) do
+    "Номера месяцев в году"
+  end
+
   def title(%{:isIndex => true}) do
     ~s/Какой месяц сейчас?/
   end
