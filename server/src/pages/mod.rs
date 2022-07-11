ed01383dbc469fa9df4761492cdb35adc74fc0c0
erdmko <erdmko@gmail.com>
2022-07-11 22:35:18 +0200

    Simple month page

--- server/src/pages/mod.rs
+++ server/src/pages/mod.rs
@@ -1,11 +1,11 @@
 mod base64;
 mod main_page;
+mod months;
 mod random;
 mod tetris;
 mod utils;
-mod months;
 pub use self::base64::base64_page_handler;
 pub use self::main_page::main_page_handler;
+pub use self::months::month_page_handler;
 pub use self::random::random_page_handler;
 pub use self::tetris::tetris_page_handler;
-pub use self::months::month_page_handler;
--- server/src/pages/months.rs
+++ server/src/pages/months.rs
@@ -2,9 +2,26 @@ use super::utils;
 use actix_web::{get, HttpRequest, Responder};
 use tera::Context;
 
+fn get_months() -> Vec<&'static str> {
+    vec![
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ]
+}
+
 #[get("/month")]
 async fn month_page_handler(req: HttpRequest) -> impl Responder {
     let mut ctx = Context::new();
-    ctx.insert("result", "Month");
-    return utils::render(req, "random.html", &ctx).await;
+    ctx.insert("months", &get_months());
+    return utils::render(req, "months.html", &ctx).await;
 }
