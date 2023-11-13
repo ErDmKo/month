1db43765b30309eb11aedf2dda45171802664ebc
Dmitrii Eroshenko <dmitrii.eroshenko@zalando.de>
2023-11-13 18:53:57 +0100

    Files

---
+++ assets/js/tennis/screen.ts
@@ -0,0 +1,23 @@
+import { bindArg, ObserverInstance, on } from "@month/utils";
+import { Commands, START_TYPE, STOP_TYPE } from "./const";
+
+export const useScreenLoock = (
+  ctx: Window,
+  observerObj: ObserverInstance<Commands, void>
+) => {
+  let lock: any = undefined;
+  observerObj(
+      bindArg(([type]: Commands) => {
+          if (type === STOP_TYPE) {
+            lock || lock.release();
+          }
+          if (type === START_TYPE) {
+            (ctx.navigator as any).wakeLock
+              .request()
+              .then((wakeLock: any) => {
+                lock = wakeLock;
+              })
+          }
+      }, on)
+  );
+}
